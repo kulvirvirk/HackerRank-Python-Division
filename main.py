91cd4c889b1f7546dd0cabb5c1c4caa3ad6e041d
kulvirvirk <kvirkdev@gmail.com>
2020-03-03 01:30:40 +0000

    added code to execute division

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+# Task
+# Read two integers and print two lines. The first line should contain integer division,  a//b . The second line should contain float division,  a/b .
+#You don't need to perform any rounding or formatting operations.
+
 
 
 a = int(input('enter a:'))
 b = int(input('enter b:'))
+
+print(a//b)  # '//' used for floor division, sometimes aka integer division
+print(a/b)  # '/' used for floating point division
